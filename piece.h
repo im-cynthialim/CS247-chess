da4dbb0783c270de05a726fa07fd46f2db255fa6
Cynthia Lim <climtung@uwaterloo.ca>
2024-07-25 18:03:01 -0400

    add ctor for Piece

--- piece.h
+++ piece.h
@@ -11,10 +11,10 @@ class Piece {
     Colour colour;
     char pieceType;
     Piece (Colour colour, char pieceType);
-    virtual vector<Move> possibleMoves(vector<vector<Piece>>, int, int) = 0;
+    virtual vector<Move*> possibleMoves(vector<vector<Piece*>>, int, int) = 0;
     
     public:
-        vector<Move> listMoves (vector<vector<Piece>>, int, int);
+        vector<Move*> listMoves (vector<vector<Piece*>>, int, int);
 };
 
 #endif
