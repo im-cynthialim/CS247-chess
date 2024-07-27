99d704373d4d481c553a3396b94515bece5ad743
Emerson Dang <edang@uwaterloo.ca>
2024-07-26 22:53:05 -0400

    add lineOfSight, modify possibleMoves

--- knight.cc
+++ knight.cc
@@ -1,4 +1,5 @@
 #include "knight.h"
+#include "king.h"
 #include "piece.h"
 #include <iostream>
 #include <vector>
@@ -7,7 +8,7 @@ using namespace std;
 
 Knight::Knight(Colour c, char PieceType): Piece::Piece{c, PieceType} {};
 
-vector<Move> Knight::possibleMoves(vector<vector<Piece*>> board, int row, int col){
+vector<Move> Knight::lineOfSight(vector<vector<Piece*>>& board, int row, int col) {
 
     vector<Move> validMoves {};
 
@@ -52,6 +53,50 @@ vector<Move> Knight::possibleMoves(vector<vector<Piece*>> board, int row, int co
         validMoves.push_back(Move{row, col, row-2, col-1});
     }
 
+    return validMoves;
+}
+
+vector<Move> Knight::possibleMoves(vector<vector<Piece*>> &board, int row, int col) {
+    vector<Move> validMoves{};
+    vector<Move> potentialMoves = lineOfSight(board, row, col);  //get general moves of a pieces
+
+    // call find king to find MY king's position
+    int kingX = 3;
+    int kingY = 6;
+    bool inCheck = false;
+    int initX = row;
+    int initY = col;
+
+    // for each general move, call line of Sight on enemy pieces + see if my king within results (i.e. check) 
+    int validateMove = 0; // index to iterate through generalMoves
+    
+    vector<vector<Piece*>> simulateBoard = board;
+
+    while (validateMove < potentialMoves.size()) {
+        simulateBoard[initX][initY] = nullptr;
+        simulateBoard[potentialMoves[validateMove].getToX()][potentialMoves[validateMove].getToY()] = this; // put rook in new potential place
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (simulateBoard[i][j] != nullptr && simulateBoard[i][j]->getColour() != this->getColour()) {
+
+                    vector<Move> enemyMoves = simulateBoard.at(i).at(j)->getLineOfSightMoves(simulateBoard, i, j);
+                    Move myKing = {i, j, kingX, kingY};
+                    auto it = find (enemyMoves.begin(), enemyMoves.end(), myKing);
+                    if (it != enemyMoves.end()) { // enemy piece checks my king
+                        inCheck = true;
+                        break; // general move is invalid
+                    }
+                }
+            }
+        }
+        if (!inCheck) { // no enemy line of sight puts my king in check for my simulated move, therefore valid move
+            validMoves.push_back(potentialMoves[validateMove]);
+        }
+        inCheck = false;
+        initX = potentialMoves[validateMove].getToX();
+        initY = potentialMoves[validateMove].getToY();
+        ++validateMove;
+    }
     return validMoves;
 }
 
