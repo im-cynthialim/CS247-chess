429983a674eff5ba6f3f40f1243e9e298a841a9f
kmandhan <kmandhan@uwaterloo.ca>
2024-07-29 13:04:11 -0400

    helperfunc

---
+++ helperFuncs.cc
@@ -0,0 +1,40 @@
+#include <vector>
+#include "enums.h"
+#include "piece.h"
+
+bool isKingInCheck(char king, const vector<vector<Piece*>>& board) {
+        Colour kingColour = WHITE;
+        if (king  == 'k') {
+            kingColour = BLACK;
+        }
+
+        int kingPosX;
+        int kingPosY;
+        for (size_t row = 0; row < board.size(); ++row) {
+            for (size_t col = 0; col < board[row].size(); ++col) {
+                Piece* piece = board[row][col];
+                if(piece != nullptr)  {
+                    if(piece->getPieceType() == king) {
+                        kingPosX = row;
+                        kingPosY = col;
+                    }
+                }
+            }
+        }
+
+        for (size_t row = 0; row < board.size(); ++row) {
+            for (size_t col = 0; col < board[row].size(); ++col) {
+                Piece* piece = board[row][col];
+                if(piece != nullptr && piece->getColour() != kingColour) { //other guys piece from the king
+                    //find all LINES OF SIGHT moves of other guys pieces on the board
+                    vector<Move> possibleMoves = board[row][col]->getLineOfSightMoves(board, row, col);
+                    for (size_t j = 0; j < possibleMoves.size(); ++j) {
+                        if(possibleMoves[j].getToX() == kingPosX && possibleMoves[j].getToY() == kingPosY) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
