eaa9c600b5015e2beb337d749d5af1e3deaf2887
kmandhan <kmandhan@uwaterloo.ca>
2024-07-30 17:36:24 -0400

    update chess.cc

--- chess.cc
+++ chess.cc
@@ -14,7 +14,7 @@ int main () {
 
     Game *game = new Game;
     new TextObserver(game);
-    new GraphicsObserver(game);
+    // new GraphicsObserver(game);
     game->notifyObservers();
 
     std::string command;
@@ -26,7 +26,7 @@ int main () {
                 delete game; // Clean up the old game
                 game = new Game(); // Create a fresh game
                 new TextObserver(game);
-                new GraphicsObserver(game);
+                // new GraphicsObserver(game);
             }
 
             std::string whitePlayer, blackPlayer;
@@ -36,41 +36,41 @@ int main () {
             game->notifyObservers();
         }
         else if (command == "setup") {
-            if (game) {
+            if (game->status == NOTSTARTED) {
                 game->setup();
             } else {
                 std::cout << "No game in progress.\n";
             }
         }
         else if (command == "move") {
-            if (game) { 
+            if (game->status == RUNNING) { 
                 game->makeMove();
                 if (game->status == WHITEWINS) {
                     whiteScore++;
                     delete game; // Game is done
                     game = new Game(); // Create a fresh game
                     new TextObserver(game);
-                    new GraphicsObserver(game);
+                    // new GraphicsObserver(game);
                 } else if (game->status == BLACKWINS) {
                     blackScore++;
                     delete game; // Game is done
                     game = new Game(); // Create a fresh game
                     new TextObserver(game);
-                    new GraphicsObserver(game);
+                    // new GraphicsObserver(game);
                 } else if (game->status == DRAW) {
                     whiteScore = whiteScore + 0.5;
                     blackScore = blackScore + 0.5;
                     delete game; // Game is done
                     game = new Game(); // Create a fresh game
                     new TextObserver(game);
-                    new GraphicsObserver(game);
+                    // new GraphicsObserver(game);
                 }
             } else {
                 std::cout << "No game in progress.\n";
             }
         } 
         else if (command == "resign") {
-            if (game) {
+            if (game->status == RUNNING) {
                 game->resign();
                 if (game->status == BLACKWINS) {
                     blackScore++;
@@ -81,7 +81,7 @@ int main () {
                 delete game; // Game is done
                 game = new Game(); // Create a fresh game
                 new TextObserver(game);
-                new GraphicsObserver(game);
+                // new GraphicsObserver(game);
             } else {
                 std::cout << "No game in progress.\n";
             }
